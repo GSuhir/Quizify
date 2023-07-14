136475588793802b295de6c15a61d5747485d307
SoloSolaire <128697724+SoloSolaire@users.noreply.github.com>
2023-07-13 19:51:32 -0700

    added quiz model

---
+++ models/Quiz.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Quiz extends Model {}
+
+Quiz.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        quiz_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        question_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'questions',
+                key: 'id'
+            }
+        },
+        answer_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'answers',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'quiz',
+    }
+)
+
+module.exports = Quiz
