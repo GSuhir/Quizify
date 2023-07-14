3fb17e1bd93194a5b50b96543b15bf004a9d98bb
SoloSolaire <128697724+SoloSolaire@users.noreply.github.com>
2023-07-13 20:30:25 -0700

    required and exported models in index.js

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const User = require('./User');
+const Quiz = require('./Quiz');
+const Question = require('./Question');
+const Answer = require('./Answer');
+
+module.exports = {
+    User,
+    Quiz,
+    Question,
+    Answer,
+};
