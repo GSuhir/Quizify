0342c23c9676ba5c1eb0d3146a5f8c2e23e000e9
SoloSolaire <128697724+SoloSolaire@users.noreply.github.com>
2023-07-18 19:51:55 -0700

    added post for user and seed debug

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.post('/', async (req, res) => {
+  try{
+    const userData = await User.create(req.body);
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
