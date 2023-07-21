c6fc6eee1bf42ce514cc5b867528ebfbfeed9c17
Tod Winchester <tod.winchester2@gmail.com>
2023-07-20 21:10:44 -0700

    questions and answered

--- seeds/answer-seed.js
+++ seeds/answer-seed.js
@@ -7,6 +7,54 @@ const answerData = [
         answerC: 'oranges',
         answerD: 'mangos',
     },
+    {
+        answerA: 'Eiffel Tower',
+        answerB: 'Statue of Liberty',
+        answerC: 'Great Wall of China',
+        answerD: 'Colosseum',
+    },
+    {
+        answerA: '100',
+        answerB: '200',
+        answerC: '400',
+        answerD: '500',
+    },
+    {
+        answerA: 'Frisbee',
+        answerB: 'Soccer',
+        answerC: 'Golf',
+        answerD: 'Bocce ball',
+    },
+    {
+        answerA: 'Wheat',
+        answerB: 'Barley',
+        answerC: 'Hops',
+        answerD: 'Rice',
+    },
+    {
+        answerA: 'Belgium',
+        answerB: 'France',
+        answerC: 'USA',
+        answerD: 'England',
+    },
+    {
+        answerA: 'Granite',
+        answerB: 'Marble',
+        answerC: 'Iron',
+        answerD: 'Diamond',
+    },
+    {
+        answerA: 'New Zealand',
+        answerB: 'Croatia',
+        answerC: 'Iceland',
+        answerD: 'Ireland',
+    },
+    {
+        answerA: 'Vermont',
+        answerB: 'Canada',
+        answerC: 'Rhode Island',
+        answerD: 'Nevada',
+    },
 ];
 
 const seedAnswers = () => Answer.bulkCreate(answerData);
--- seeds/question-seed.js
+++ seeds/question-seed.js
@@ -3,6 +3,39 @@ const { Question } = require('../models');
 const questionData = [
     {
         question: 'What is my favorite fruit?',
+        quiz_id:1
+    },
+    {
+        question: 'What is the most visited tourist attraction in the world?',
+        quiz_id:2
+    },
+    {
+        question: 'On average, how many seeds are located on the outside of a strawberry?',
+        quiz_id:3
+    } ,
+    {
+        question: 'What sport has been played on the moon?',
+        quiz_id:4
+    }, 
+    {
+        question: 'Which cereal grain is the most commonly used in beer?',
+        quiz_id:5
+    },
+     {
+        question: 'From which country do French fries originate?',
+        quiz_id:6
+    }, 
+    {
+        question: 'What is the hardest natural substance in the world?',
+        quiz_id:7
+    },
+    {
+        question: 'In what country was The Lord of the Rings trilogy shot?',
+        quiz_id:8
+    },
+    {
+        question: 'Which is the smallest state in the United States by size?',
+        quiz_id:9
     }
 ];
 
