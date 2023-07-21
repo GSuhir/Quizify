0727207ddc2116f5b378756257cff0a07a992636
SoloSolaire <128697724+SoloSolaire@users.noreply.github.com>
2023-07-20 19:25:08 -0700

    added correctAnswer row to the answer table

--- seeds/answer-seed.js
+++ seeds/answer-seed.js
@@ -6,7 +6,17 @@ const answerData = [
         answerB: 'bananas',
         answerC: 'oranges',
         answerD: 'mangos',
+        correctAnswer: 'answerD',
+        question_id: 1
     },
+    {
+        answerA: 'chassis',
+        answerB: 'engine',
+        answerC: 'tires',
+        answerD: 'catalytic converter',
+        correctAnswer: 'answerC',
+        question_id: 2
+    }
 ];
 
 const seedAnswers = () => Answer.bulkCreate(answerData);
